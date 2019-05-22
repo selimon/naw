111ea522bc96f1461a7e44bb461549696b3415d6
selimon <dida.z876@gmail.com>
2019-05-22 13:34:15 +0300

    defewf

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,39 @@
-// menu-mobile
+// show-carusel 
 $(document).ready(function() {
-    $('.navbar-toggler').click(function() {
-        $('.navbar-mobile-menu').addClass('open');
-    });
-    $('.close').click(function() {
-        $('.navbar-mobile-menu').removeClass('open');
-    });
-    $(".navbar-toggler").click(function() {
-        $(".navbar-toggler").addClass('op');
-    });
-    $(".close").click(function() {
-        $(".navbar-toggler").removeClass('op');
-    });
-
+    $('.slide-one').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        pagination: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
+$(document).ready(function() {
+    $('.slide-2').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        pagination: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
 });
