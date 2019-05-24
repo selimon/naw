248a8632a6a027bf6458f60450ef7fb4540ec6a8
selimon <dida.z876@gmail.com>
2019-05-24 20:42:44 +0300

    dgdgd

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,67 @@
-// menu-mobile
+// show-carusel 
+$(document).ready(function() {
+    $('.slide-one').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        pagination: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
+$(document).ready(function() {
+    $('.slide-2').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        pagination: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
+$(document).ready(function() {
+    $('.slide-mobile').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        pagination: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+});
+
+// scroll 
+$(document).ready(function() {
+
+});
+// menu 
 $(document).ready(function() {
     $('.navbar-toggler').click(function() {
         $('.navbar-mobile-menu').addClass('open');
@@ -13,123 +76,4 @@ $(document).ready(function() {
         $(".navbar-toggler").removeClass('op');
     });
 
-});
-
-
-
-
-
-// show-carusel 
-$(document).ready(function() {
-
-    // product-gallery:
-    var sync1 = $("#sync1");
-    var sync2 = $("#sync2");
-
-    sync1.owlCarousel({
-        items: 1,
-        dots: false,
-        nav: true,
-        smartSpeed: 1000,
-        margin: 15,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    });
-
-    sync1.on('changed.owl.carousel', function(event) {
-        var current = event.item.index;
-        sync2.trigger('to.owl.carousel', [current, 300, true]);
-        sync2
-            .find("a")
-            .removeClass("synced")
-            .eq(current)
-            .addClass("synced");
-    });
-
-    sync2.on('click', 'a', function() {
-        sync1.trigger('to.owl.carousel', [$(this).index(), 300, true]);
-    });
-    // end product-gallery
-
-});
-// owl-carusel 
-$(document).ready(function() {
-    $(".slide-1").owlCarousel({
-        loop: true,
-        nav: true,
-        margin: 10,
-        navigationText: ["", ""],
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 3
-            }
-        }
-    })
-});
-// gallery-carousel
-$(function() {
-    $('.slide-2').owlCarousel({
-        loop: true,
-        nav: true,
-        margin: 10,
-        navigationText: ["", ""],
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            700: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            },
-            1500: {
-                items: 4
-            }
-        }
-    })
-});
-// raccal
-$(function() {
-    $('.slide-3').owlCarousel({
-        loop: true,
-        nav: true,
-        margin: 10,
-        navigationText: ["", ""],
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            700: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            },
-            1500: {
-                items: 4
-            }
-        }
-    })
 });
