ae5f4b5b9d65ab8598f8479e5f6397df0e32964e
Selimon <you@example.com>
2019-08-10 20:03:00 +0300

    mobile

---
+++ js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+ $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
+
+// scroll 
+// $(window).scroll(function() {    
+//     var scroll = $(window).scrollTop();
+
+//     if (scroll >= 370) {
+//         $(".text-animated h1").addClass("active");
+//     } else {
+//         $(".text-animated h1").removeClass("active");
+//     }
+// });
