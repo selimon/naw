c277129f2b1936589dbe23a11560b297cb4e6bb8
Selimon <you@example.com>
2019-08-14 14:11:04 +0300

    dfdg

--- js/main.js
+++ js/main.js
@@ -6,10 +6,22 @@ $(document).ready(function () {
     $('.animated-icon1').toggleClass('open');
   });
 });
+
 jQuery(document).ready(function($) {
 	$('.first-button').on('click', function(){
 		$('.navbar').toggleClass('active');
 	})
+});
+
+// scrol-active 
+jQuery(window).scroll(function() { 
+    var the_top = jQuery(document).scrollTop();
+    if (the_top > 0) {
+        jQuery('.navbar').addClass('scroll');
+    }
+    else {
+        jQuery('.navbar').removeClass('scroll');
+    }
 });
 // owl-carousel
 $('.carousel-1').owlCarousel({
@@ -17,6 +29,8 @@ $('.carousel-1').owlCarousel({
     margin:0,
     nav:false,
     dots: true,
+    autoplay: true,
+    autoplayHoverPause: true,
     responsive:{
         0:{
             items:1
@@ -28,4 +42,14 @@ $('.carousel-1').owlCarousel({
             items:1
         }
     }
-})
+});
+
+// carousel-cafe 
+$('.carousel-cafe').owlCarousel({
+    nav:true,
+    pagination:false,
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    items:3,
+});
