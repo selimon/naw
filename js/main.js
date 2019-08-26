545015c269c50e923fd0abca664f29976daccaf0
Selimon <you@example.com>
2019-08-26 19:25:20 +0300

    feg4g

--- js/main.js
+++ js/main.js
@@ -95,3 +95,21 @@ function syncPosition2(el) {
         slider.data("owl.carousel").to(number, 100, true);
     }
 }
+
+
+// menu 
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
