ff1ac61acdb09494d6c6790b3e0eca12d5145c54
Selimon <you@example.com>
2019-10-03 11:15:28 +0300

    site

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.clients').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ["",""],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
