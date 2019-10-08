b907e8451d056cab5fdfbdeb17799138fc535cfa
Selimon <you@example.com>
2019-10-08 19:48:05 +0300

    gfdg

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// preloader 
-$(window).on('load', function () {
-    $('#load').delay(1000).fadeOut('slow');
-});
 
 // slick 
 $('.sl').slick({
@@ -9,6 +5,8 @@ $('.sl').slick({
   infinite: true,
   speed: 300,
   slidesToShow: 5,
+  autoplay: true,
+  autoplaySpeed: 3000,
   slidesToScroll: 5,
   responsive: [
     {
@@ -28,7 +26,7 @@ $('.sl').slick({
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 575,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
@@ -39,64 +37,7 @@ $('.sl').slick({
     // instead of a settings object
   ]
 });
-// owl-carousel> on index.html
-$('.clients').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    navText: ["<img src='images/left1.svg'>","<img src='images/right1.svg'>"],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-});
 
-
-// img
-function theRotator() {
-    // Устанавливаем прозрачность всех картинок в 0
-    $('div#rotator ul li').css({opacity: 0.0});
- 
-    // Берем первую картинку и показываем ее (по пути включаем полную видимость)
-    $('div#rotator ul li:first').css({opacity: 1.0});
- 
-    // Вызываем функцию rotate для запуска слайдшоу, 5000 = смена картинок происходит раз в 5 секунд
-    setInterval('rotate()',5000);
-}
- 
-function rotate() { 
-    // Берем первую картинку
-    var current = ($('div#rotator ul li.show')?  $('div#rotator ul li.show') : $('div#rotator ul li:first'));
- 
-    // Берем следующую картинку, когда дойдем до последней начинаем с начала
-    var next = ((current.next().length) ? ((current.next().hasClass('show')) ? $('div#rotator ul li:first') :current.next()) : $('div#rotator ul li:first'));   
- 
-    // Расскомментируйте, чтобы показвать картинки в случайном порядке
-    // var sibs = current.siblings();
-    // var rndNum = Math.floor(Math.random() * sibs.length );
-    // var next = $( sibs[ rndNum ] );
- 
-    // Подключаем эффект растворения/затухания для показа картинок, css-класс show имеет больший z-index
-    next.css({opacity: 0.0})
-    .addClass('show')
-    .animate({opacity: 1.0}, 1000);
- 
-    // Прячем текущую картинку
-    current.animate({opacity: 0.0}, 1000)
-    .removeClass('show');
-};
- 
-$(document).ready(function() {      
-    // Запускаем слайдшоу
-    theRotator();
-});
 
 
 
