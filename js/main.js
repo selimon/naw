e2b0bb48819259b5f3aa4afd93519a9af98797f2
Selimon <you@example.com>
2019-10-08 23:01:19 +0300

    dddd

--- js/main.js
+++ js/main.js
@@ -10,22 +10,6 @@ $('.sl').slick({
   slidesToScroll: 5,
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
       breakpoint: 575,
       settings: {
         slidesToShow: 1,
